563bd6799790b0e6167b8524a9a01d9e201f631e
prathamesh-pichkate <prathameshpichkate@gmail.com>
2025-01-21 23:24:01 +0530

    Updated the file for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,12 @@ dotenv.config();
 import connectDB from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 import { app,server } from './lib/socket.js';
 
 
 const PORT  = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({
     origin:"http://localhost:5173",
@@ -19,6 +21,14 @@ app.use(express.json({ limit: "10mb" })); //allow to extract the data from the b
 app.use(cookieParser()); //helps to parse the cookie data
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 server.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}` );
