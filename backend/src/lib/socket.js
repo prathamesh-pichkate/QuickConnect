15ab064e2e9acadf26cb345fcf5879652eab1ffa
prathamesh-pichkate <prathameshpichkate@gmail.com>
2025-03-20 13:47:46 +0530

    Updated the code

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,38 +1,37 @@
-import express from 'express';
+import { Server } from "socket.io";
 import http from "http";
-import { Server } from 'socket.io';
+import express from "express";
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
-    cors: {
-        origin: ["http://localhost:5173", "http://13.201.134.175:5173"], // Allow both local & EC2 frontend
-        methods: ["GET", "POST"],  // Allow necessary HTTP methods
-        credentials: true  // Enable sending cookies & headers
-    }
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
 });
 
-
-export function getReceiverSocketId(userId){
-    return userSocketMap[userId];
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
 }
 
-//store the online users
-const userSocketMap = {};
-io.on('connection', (socket) => {
-    console.log('a user connected',socket.id);
+// used to store online users
+const userSocketMap = {}; // {userId: socketId}
 
-    const userId = socket.handshake.query.userId;
-    if(userId) userSocketMap[userId] = socket.id;
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
 
-    //io.emit() is used to send events to all the connected clients
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected', socket.id);
-        delete userSocketMap[userId];
-        io.emit("getOnlineUsers",Object.keys(userSocketMap));
-    });
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
+});
 
-export {io,server,app};
+export { io, app, server };
